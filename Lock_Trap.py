7bf8d0a20fe4c60e22d305355fbce1866f0ae50a
Danijel Milosevic <35433167+DanijelMi@users.noreply.github.com>
2018-10-16 11:58:37 +0200

    Update Lock_Trap.py

--- Lock_Trap.py
+++ Lock_Trap.py
@@ -3,26 +3,50 @@ from threading import Thread    #
 import pynput.mouse as ms       #
 import pynput.keyboard as kb    #
 from time import gmtime, strftime, sleep
+import argparse
 
-SCREENSHOT_DIR = "/home/danijel/desktop"
+def parseArgs():
+    parser = argparse.ArgumentParser(
+        usage='%(prog)s [options]', 
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter, 
+        description="Locks the system and captures a webcam photo as soon as user input is detected.",
+        epilog="Example: %(prog)s -t 3 -p /home/user/Desktop")
+    parser.add_argument('-v', '--verbose', action='store_const',
+                        const=True, default=False, help='For debugging purposes.')
+    parser.add_argument('-l', '--no-lock', action='store_const',
+                        const=True, default=False, help='Disables locking')
+    parser.add_argument('-n','--no-video', action='store_const',
+                        const=True, default=False, help='Disables image capturing')
+    parser.add_argument('-p', '--path', default='./', help='File path to captured images')
+    parser.add_argument('-t', '--arm-time', type=int, default=3, help='Seconds before activating')
+    #args = parser.parse_args()
+    args = vars(parser.parse_args())
+    return args['verbose'], args['no_lock'], args['no_video'], args['path'], args['arm_time']
 
-sleep(1)     # Seconds of waiting before starting the script
+
+args=parseArgs()
+print(args)
+sleep(args[4])       # Seconds of waiting before starting the script
 
 def bash_command(cmd):                              # 
     subprocess.Popen(['/bin/bash', '-c', cmd])      #
 
+
 def on_action(*args):                               # Gets called when a keyboard
    sys.exit()                                       # or mouse press happens
+
 
 def keyboard_listen():                      # Defines a listener for keyboard
     with kb.Listener(                       # presses
             on_press=on_action) as listener:
         listener.join()
 
+
 def mouse_listen():                         # Defines a listener for mouse
     with ms.Listener(                       # presses
             on_click=on_action) as listener:
         listener.join()
+
 
 t_kb=Thread(target=keyboard_listen, args=())# Sets the listener on a new thread.
 t_kb.setDaemon(True)                        # Daemons get killed if the caller (main) process
@@ -36,12 +60,8 @@ while(t_kb.isAlive() and t_ms.isAlive()):   # If either thread died, then a mous
     sleep(1)                           # triggered. Therefore stop checking and exit loop
 
 
-#captureCommand = "mplayer -vo jpeg:outdir=/var/www/html -frames 1 tv://"
-captureCommand = "mplayer -vo jpeg:outdir={}/{} -frames 1 tv://"
-captureCommand = captureCommand.format(SCREENSHOT_DIR, strftime("%Y-%m-%d_%H-%M-%S", gmtime()))
+captureCommand = "mplayer -vo jpeg:outdir={}/{} -frames 1 tv://".format(args[3], strftime("%Y-%m-%d_%H-%M-%S", gmtime()))
 print(captureCommand)
-#bash_command(captureCommand)
-#bash_command("mplayer -vo png -frames 1 tv://")
-#bash_command("loginctl lock-session")
-
+bash_command(captureCommand) if not args[2] else False          # Captures image
+bash_command("loginctl lock-session") if not args[1] else False # Locks session
 sys.exit()                                  # Closes main process and its daemons too.
